8ab9197b56b2dc278e90fc3ef53781d14151b070
ranex <ranex@localhost.localdomain>
2019-12-27 22:28:21 +0530

    checked PORT error

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const hbs = require('hbs');
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 
 const app = express();
